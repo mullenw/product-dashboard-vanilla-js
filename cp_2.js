64b2ae09fbf9b958c189a52c4ef3c8f1fd9eaf68
Wyatt Mullen <mullenw@usf.edu>
2025-10-17 12:36:31 -0400

    async function

--- cp_2.js
+++ cp_2.js
@@ -14,3 +14,18 @@ function fetchProductsThen() {
       console.log("Error with .then() fetch:", error);
     });
 }
+
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch("https://www.course-api.com/javascript-store-products");
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function displayProducts(products) {
+  const container = document.getElementById("product-container");
+  container.innerHTML = "";
+  const firstFive = products.slice(0, 5);
